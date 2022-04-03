22e1d7bcd897c611b0673e53d0c37787e84b927b
mrusb <mrusboy@mail.ru>
2022-04-03 14:35:29 +0300

    add 4.1

--- 4.1.cpp
+++ 4.1.cpp
@@ -7,7 +7,6 @@
 #include <time.h>
 #include <iomanip>
 #include <windows.h>
-#include <vector>
 
 
 using namespace std;
@@ -35,7 +34,7 @@ struct Order {
     string fio;
     string number;
     int currentcountofproducts = 0;
-    int listofproducts[maxcountofproducts][2];
+    int listofproducts[N][2];
 };
 
 struct SuperMarket {
@@ -50,19 +49,19 @@ struct SuperMarket {
 
     void AddOrder();
 
-    void DelOrder();
+    void DelOrder(int id);
 
-    void Print();
 
     boolean AddProduct(int numberoforder, int idproduct, int count);
 
-    void CalculatingBill();
+    unsigned int CalculatingBill(int id);
 };
 
 boolean SuperMarket::AddProduct(int numberoforder, int idproduct, int count) {
-    if (products[idproduct].count > 0) {
-        orders[numberoforder].listofproducts[idproduct][0] = idproduct;
-        orders[numberoforder].listofproducts[idproduct][1] = count;
+    if (products[idproduct].count >= count && idproduct >= 0 && idproduct < 6) {
+        orders[numberoforder].listofproducts[orders[numberoforder].currentcountofproducts][0] = idproduct;
+        orders[numberoforder].listofproducts[orders[numberoforder].currentcountofproducts][1] = count;
+        orders[numberoforder].currentcountofproducts++;
         products[idproduct].count -= count;
         return true;
     }
@@ -85,9 +84,56 @@ void SuperMarket::CreateListOfOrders() {
         orders[countoforders].date += to_string(rand() % 12 + 1) += "-" + to_string(rand() % 27 + 1);
         orders[countoforders].fio = names[countoforders];
         orders[countoforders].number = numbers[countoforders];
-        while (!AddProduct(countoforders, rand() % maxcountofproducts, rand() % 10));
+        while (AddProduct(countoforders, rand() % maxcountofproducts + 1, rand() % 9 + 1));
         countoforders++;
     }
+}
+
+void SuperMarket::AddOrder() {
+    int id;
+    string date, fio, number;
+    cout << "Enter id, date, fio and contact number:" << endl;
+    cin >> id >> date >> fio >> number;
+    orders[countoforders].id = id;
+    orders[countoforders].date = date;
+    orders[countoforders].fio = fio;
+    orders[countoforders].number = number;
+    cout << "Enter id and count of products:\nIf you entered all wishes product enter '-1'." << endl;
+    int idproduct, countproduct;
+    while (true) {
+        cin >> idproduct;
+        if (idproduct == -1) break;
+        cin >> countproduct;
+        if (AddProduct(id, idproduct, countproduct)) { cout << "Successfully!"; }
+        else { cout << "Not id or count of product"; }
+    }
+    cin >> id >> date >> fio >> number;
+    countoforders++;
+}
+
+void SuperMarket::DelOrder(int id) {
+
+    for (int i = 0; i < orders[id].currentcountofproducts; i++) {
+        products[orders[id].listofproducts[i][0]].count += orders[id].listofproducts[i][1];
+    }
+    orders[id].currentcountofproducts = 0;
+    for (int i = id; i < countoforders-1; ++i) {
+        orders[i] = orders[i+1];
+    }
+    countoforders--;
+
+}
+
+unsigned int SuperMarket::CalculatingBill(int id) {
+    unsigned int sum = 0;
+    if (orders[id].id != -1) {
+        for (int i = 0; i < orders[id].currentcountofproducts; i++) {
+            sum += products[orders[id].listofproducts[i][0]].price * orders[id].listofproducts[i][1];
+//            cout << products[orders[id].listofproducts[i][0]].price << endl;
+//            cout << orders[id].listofproducts[i][1] << endl;
+        }
+    } else cout << "Order with this id was deleted :c" << endl;
+    return sum;
 }
 
 
@@ -100,10 +146,38 @@ int main() {
     SuperMarket superMarket;
     superMarket.CreateListOfProcucts();
     superMarket.CreateListOfOrders();
-//    int n = 0;
-//    while (n < countoforders) {
-//        cout << superMarket.orders[n].date << endl;
+
+    int n = 0;
+//    int id = 0;
+//    cout << "Bill for id " << id << ": " << superMarket.CalculatingBill(0) << endl;
+
+//    while (n < maxcountofproducts) {
+//        cout << superMarket.products[n].id << "\t" << superMarket.products[n].count << endl;
 //        n++;
 //    }
+    while (n < countoforders) {
+        cout << superMarket.orders[n].id << "\t" << superMarket.orders[n].fio << endl;
+        n++;
+    }
+    cout << "-------------------------------------" << endl;
+
+    superMarket.DelOrder(2);
+
+    n = 0;
+    while (n < countoforders) {
+        cout << superMarket.orders[n].id << "\t" << superMarket.orders[n].fio << endl;
+        n++;
+    }
+
+//    cout << "-------------------------------------" << endl;
+
+    n = 0;
+
+//    while (n < superMarket.orders[id].currentcountofproducts) {
+//        cout << superMarket.orders[id].listofproducts[n][0] << "\t" << superMarket.orders[id].listofproducts[n][1]
+//             << endl;
+//        n++;
+//    }
+
 
 }
