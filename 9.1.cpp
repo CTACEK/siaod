628ff8e67d2b58d6ea7a2f2d0c241f47ec2f1999
ctacek <mrusboy@mail.ru>
2022-05-21 15:31:24 +0300

    final 9

--- 9.1.cpp
+++ 9.1.cpp
@@ -5,8 +5,6 @@
 #include <iomanip>
 #include <string>
 
-//orders = (Order *) calloc(maxcountoforder, sizeof(Order));
-//orders = (Order *) realloc(orders, countoforders * sizeof(Order));
 
 using namespace std;
 
@@ -16,12 +14,11 @@ string numbers[4] = {"+7(936)209-93-10", "+7(981)942-26-08", "+7(941)802-26-23",
 struct User {
     string number;
     string address;
-
-    User(string number, string address);
 };
 
 struct PhoneBook {
-    int count_of_users = 1;
+    int count_of_users = 0;
+    int max_count_of_users = 4;
     User Users[4];
 
     void add_user(User);
@@ -32,51 +29,51 @@ struct PhoneBook {
 
     string find_user(string number);
 
-    string find_user_barier(string number);
+    string find_user_barrier(string number);
 
 };
 
 
 void PhoneBook::print_phonebook() {
     cout << "Table of users of phonebook" << "      \n";
-    cout << "__________________________________________\n";
-    cout << left << setw(20) << "Number of user";
+    cout << "______________________________________________\n";
+    cout << left << setw(22) << "Number of user[keys]";
     cout << "|";
-    cout << left << setw(20) << "Data of user";
+    cout << left << setw(22) << "Data of user[values]";
     cout << "|" << endl;
-    cout << "____________________|____________________|" << endl;
+    cout << "______________________|______________________|" << endl;
+//    cout << Users[0].number << endl;
     for (User user: Users) {
-        cout << left << setw(20) << user.number;
+        cout << left << setw(22) << user.number;
         cout << "|";
-        cout << left << setw(20) << user.address;
+        cout << left << setw(22) << user.address;
         cout << "|" << endl;
-        cout << "____________________|____________________|" << endl;
+        cout << "______________________|______________________|" << endl;
     }
+    cout << endl;
 }
 
 void PhoneBook::create_randomtable() {
-    int k = 0;
-    for (User user: Users) {
-        user.number = numbers[k];
-        user.address = names[k];
-        k++;
+    for (int i = 0; i < 4; i++) {
+        Users[i].number = numbers[count_of_users];
+        Users[i].address = names[count_of_users];
+        count_of_users++;
     }
 }
 
 string PhoneBook::find_user(string number) {
-    for (User user: Users) {
-        if (user.number == number) return user.address;
-    }
+    for (User user: Users) if (user.number == number) return user.address;
+    return "None";
 }
 
-string PhoneBook::find_user_barier(string number) {
+string PhoneBook::find_user_barrier(string number) {
     int size = count_of_users;
     if (size != 0) {
         string last = Users[size - 1].address;
-        Users[size - 1].address = number;
-        int i;
-        for (i = 0; Users[i].address != number; ++i)
-            Users[size - 1].address = last;
+        Users[size - 1].number = number;
+        int i = 0;
+        for (i = 0; Users[i].number != number; ++i)
+            Users[size - 1].number = last;
         if (i != (size - 1) || number == last) return Users[i].address;
     }
     return "None";
@@ -84,7 +81,13 @@ string PhoneBook::find_user_barier(string number) {
 
 
 int main() {
-
-
+    PhoneBook phoneBook;
+    phoneBook.create_randomtable();
+    phoneBook.print_phonebook();
+    cout << "Finding user with linear finding" << endl;
+    cout << phoneBook.find_user("+7(941)802-26-23") << endl;
+    cout << endl;
+    cout << "Finding user with barrier finding" << endl;
+    cout << phoneBook.find_user_barrier("+7(941)802-26-23") << endl;
     return 0;
 }
--- 9.2.cpp
+++ 9.2.cpp
@@ -5,72 +5,63 @@
 #include <iomanip>
 #include <string>
 
-//orders = (Order *) calloc(maxcountoforder, sizeof(Order));
-//orders = (Order *) realloc(orders, countoforders * sizeof(Order));
 
 using namespace std;
 
 string names[4] = {"Ryan Brown", "Davis Ostin", "Mark Mandel", "John Jobs"};
 string numbers[4] = {"+7(936)209-93-10", "+7(981)942-26-08", "+7(941)802-26-23", "+7(970)767-98-51"};
 
-int HashFunctionHorner(string& s, int table_size, const int key)
-{
+int HashFunctionHorner(string &s, int table_size, const int key) {
     int hash_result = 0;
-    for (int i = 0; s[i] != s.size(); ++i)
-    {
+    for (int i = 0; s[i] != s.size(); ++i) {
         hash_result = (key * hash_result + s[i]) % table_size;
     }
     hash_result = (hash_result * 2 + 1) % table_size;
     return hash_result;
 }
 
-struct HashFunction1
-{
-    int operator()(string& s, int table_size) const
-    {
+struct HashFunction1 {
+    int operator()(string &s, int table_size) const {
         return HashFunctionHorner(s, table_size, table_size - 1);
     }
 };
 
-struct HashFunction2
-{
-    int operator()(string& s, int table_size) const
-    {
+struct HashFunction2 {
+    int operator()(string &s, int table_size) const {
         return HashFunctionHorner(s, table_size, table_size + 1);
     }
 };
 
-template <class THash1 = HashFunction1, class THash2 = HashFunction2>
-class HashTable
-{
-    static const int default_size = 8;
+template<class THash1 = HashFunction1, class THash2 = HashFunction2>
+class HashTable {
+    static const int default_size = 4;
     constexpr static const double rehash_size = 0.75;
 
     struct User {
+        string number;
         string address;
         bool state;
-        User(string address_): address(address_), state(true) {}
+
+        User(string number_, string address_) : number(number_), state(true), address(address_) {}
     };
 
-    User** arr;
+    User **arr;
     int size;
     int buffer_size;
     int size_all_non_nullptr;
 
-    void Resize()
-    {
+    void resize() {
         int past_buffer_size = buffer_size;
         buffer_size *= 2;
         size_all_non_nullptr = 0;
         size = 0;
-        User** arr2 = new User * [buffer_size];
+        User **arr2 = new User *[buffer_size];
         for (int i = 0; i < buffer_size; ++i)
             arr2[i] = nullptr;
         swap(arr, arr2);
-        for (int i = 0; i < past_buffer_size; ++i)
-        {
+        for (int i = 0; i < past_buffer_size; ++i) {
             if (arr2[i] && arr2[i]->state)
-                Add(arr2[i]->value);
+                add(arr2[i]->number, arr2[2]->address);
         }
         for (int i = 0; i < past_buffer_size; ++i)
             if (arr2[i])
@@ -78,18 +69,16 @@ class HashTable
         delete[] arr2;
     }
 
-    void Rehash()
-    {
+    void rehash() {
         size_all_non_nullptr = 0;
         size = 0;
-        User** arr2 = new User * [buffer_size];
+        User **arr2 = new User *[buffer_size];
         for (int i = 0; i < buffer_size; ++i)
             arr2[i] = nullptr;
         swap(arr, arr2);
-        for (int i = 0; i < buffer_size; ++i)
-        {
+        for (int i = 0; i < buffer_size; ++i) {
             if (arr2[i] && arr2[i]->state)
-                Add(arr2[i]->value);
+                add(arr2[i]->number, arr2[i]->address);
         }
         for (int i = 0; i < buffer_size; ++i)
             if (arr2[i])
@@ -97,65 +86,60 @@ class HashTable
         delete[] arr2;
     }
 
+
 public:
-    HashTable()
-    {
+    HashTable() {
         buffer_size = default_size;
         size = 0;
         size_all_non_nullptr = 0;
-        arr = new User*[buffer_size];
+        arr = new User *[buffer_size];
         for (int i = 0; i < buffer_size; ++i)
             arr[i] = nullptr;
     }
-    ~HashTable()
-    {
+
+    ~HashTable() {
         for (int i = 0; i < buffer_size; ++i)
             if (arr[i])
                 delete arr[i];
         delete[] arr;
     }
-    bool Add(string &value, const THash1& hash1 = THash1(),const THash2& hash2 = THash2())
-    {
+
+    bool add(string &key, string &address, const THash1 &hash1 = THash1(), const THash2 &hash2 = THash2()) {
         if (size + 1 > int(rehash_size * buffer_size))
-            Resize();
+            resize();
         else if (size_all_non_nullptr > 2 * size)
-            Rehash();
-        int h1 = hash1(value, buffer_size);
-        int h2 = hash2(value, buffer_size);
+            rehash();
+        int h1 = hash1(key, buffer_size);
+        int h2 = hash2(key, buffer_size);
         int i = 0;
         int first_deleted = -1;
-        while (arr[h1] != nullptr && i < buffer_size)
-        {
-            if (arr[h1]->value == value && arr[h1]->state)
+        while (arr[h1] != nullptr && i < buffer_size) {
+            if (arr[h1]->number == key && arr[h1]->state)
                 return false;
             if (!arr[h1]->state && first_deleted == -1)
                 first_deleted = h1;
             h1 = (h1 + h2) % buffer_size;
             ++i;
         }
-        if (first_deleted == -1)
-        {
-            arr[h1] = new User(value);
+        if (first_deleted == -1) {
+            arr[h1] = new User(key, address);
             ++size_all_non_nullptr;
-        }
-        else
-        {
-            arr[first_deleted]->value = value;
+        } else {
+            arr[first_deleted]->number = key;
             arr[first_deleted]->state = true;
         }
         ++size;
         return true;
     }
-    bool Remove(string  &value, const THash1& hash1 = THash1(), const THash2& hash2 = THash2())
-    {
+
+    bool remove(string &value, const THash1 &hash1 = THash1(), const THash2 &hash2 = THash2()) {
         int h1 = hash1(value, buffer_size);
         int h2 = hash2(value, buffer_size);
         int i = 0;
-        while (arr[h1] != nullptr && i < buffer_size)
-        {
-            if (arr[h1]->value == value && arr[h1]->state)
-            {
+        while (arr[h1] != nullptr && i < buffer_size) {
+            if (arr[h1]->number == value && arr[h1]->state) {
                 arr[h1]->state = false;
+                arr[h1]->address = "";
                 --size;
                 return true;
             }
@@ -164,25 +148,29 @@ public:
         }
         return false;
     }
-    bool Find(string &value, const THash1& hash1 = THash1(), const THash2& hash2 = THash2())
-    {
+
+    string find(string &value, const THash1 &hash1 = THash1(), const THash2 &hash2 = THash2()) {
         int h1 = hash1(value, buffer_size);
         int h2 = hash2(value, buffer_size);
         int i = 0;
-        while (arr[h1] != nullptr && i < buffer_size)
-        {
-            if (arr[h1]->value == value && arr[h1]->state)
-                return true;
+        while (arr[h1] != nullptr && i < buffer_size) {
+            if (arr[h1]->number == value && arr[h1]->state)
+                return arr[h1]->address;
             h1 = (h1 + h2) % buffer_size;
             ++i;
         }
-        return false;
+        return "None";
     }
 };
 
 
 int main() {
+    HashTable<HashFunction1,HashFunction2> hashTable;
+    hashTable.add((string &) numbers[0], (string &) names[0]);
+    hashTable.add((string &) numbers[1], (string &) names[1]);
+    hashTable.remove((string &) numbers[0]);
 
-
+    cout << hashTable.find((string&) numbers[0]) << endl;
+    cout << hashTable.find((string&) numbers[1]);
     return 0;
 }
